4933b838faa9f7f1a493e446e22c3149b5b99ee7
expressuser <adityasaran2@gmail.com>
2021-11-18 11:24:57 -0600

    added teleop

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous.java
@@ -1,2 +1,73 @@
-package org.firstinspires.ftc.teamcode;public class Autonomous {
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+
+
+@com.qualcomm.robotcore.eventloop.opmode.Autonomous(name = "Autonomous")
+public class Autonomous extends LinearOpMode {
+
+    private DcMotorEx LF = null;
+    private DcMotorEx LB = null;
+    private DcMotorEx RF = null;
+    private DcMotorEx RB = null;
+
+    private final double encoderConstant = 45;
+
+    public void runOpMode() {
+        LF = hardwareMap.get(DcMotorEx.class, "left_front");
+        LB = hardwareMap.get(DcMotorEx.class, "left_back");
+        RF = hardwareMap.get(DcMotorEx.class, "right_front");
+        RB = hardwareMap.get(DcMotorEx.class, "right_back");
+
+        LF.setDirection(DcMotorEx.Direction.FORWARD);
+        LB.setDirection(DcMotorEx.Direction.FORWARD);
+        // reverse right side
+        RF.setDirection(DcMotorEx.Direction.REVERSE);
+        RB.setDirection(DcMotorEx.Direction.REVERSE);
+
+        LF.setMode(DcMotorEx.RunMode.STOP_AND_RESET_ENCODER);
+        LB.setMode(DcMotorEx.RunMode.STOP_AND_RESET_ENCODER);
+        RF.setMode(DcMotorEx.RunMode.STOP_AND_RESET_ENCODER);
+        RB.setMode(DcMotorEx.RunMode.STOP_AND_RESET_ENCODER);
+
+        LF.setMode(DcMotorEx.RunMode.RUN_TO_POSITION);
+        LB.setMode(DcMotorEx.RunMode.RUN_TO_POSITION);
+        RF.setMode(DcMotorEx.RunMode.RUN_TO_POSITION);
+        RB.setMode(DcMotorEx.RunMode.RUN_TO_POSITION);
+
+        waitForStart();
+
+        while (opModeIsActive()) {
+            driveForward(12, 0.25);
+        }
+    }
+    public void quitDriving() {
+        LF.setPower(0);
+        LB.setPower(0);
+        RF.setPower(0);
+        RB.setPower(0);
+    }
+
+    public void driveForward(double distance, double speed) {
+        LF.setTargetPosition((int) (distance * encoderConstant));
+        LB.setTargetPosition((int) (distance * encoderConstant));
+        RF.setTargetPosition((int) (distance * encoderConstant));
+        RB.setTargetPosition((int) (distance * encoderConstant));
+
+        LF.setPower(speed);
+        LB.setPower(speed);
+        RF.setPower(speed);
+        RB.setPower(speed);
+
+        while (LF.isBusy() && LB.isBusy() && RF.isBusy() && RB.isBusy()) {
+
+        }
+
+        quitDriving();
+
+    }
+
+
 }
