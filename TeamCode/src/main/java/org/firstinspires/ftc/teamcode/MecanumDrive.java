d890e0c6eed801386ff6a53319ef8cb7ee215e9b
DrUnlimitedGG <94410378+DrUnlimitedGG@users.noreply.github.com>
2021-11-20 12:41:06 -0600

    added stuff to teleop

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumDrive.java
@@ -18,7 +18,7 @@ public class MecanumDrive extends OpMode {
     public DcMotorEx carousel;
     //public Servo carousel;
 
-    private ElapsedTime runtime;
+    private ElapsedTime runtime = new ElapsedTime();
 
 
     @Override
@@ -65,24 +65,35 @@ public class MecanumDrive extends OpMode {
      */
     @Override
     public void loop() {
-        double lx = gamepad1.left_stick_x;
-        double ly = -gamepad1.left_stick_y;
-        double rx = gamepad1.right_stick_x;
+        double lx = gamepad2.left_stick_x;
+        double ly = -gamepad2.left_stick_y;
+        double rx = gamepad2.right_stick_x;
 
-        telemetry.addData("LX: ", lx);
-        telemetry.addData("LY: ", ly);
-        telemetry.addData("RX: ", rx);
-        telemetry.update();
 
+        double denominator = Math.max(Math.abs(ly) + Math.abs(lx) + Math.abs(rx), 1);
+
+        double left_front_power = (ly + lx + rx)/denominator;
+        double left_back_power = (ly - lx + rx)/denominator;
+        double right_front_power = (ly - lx - rx)/denominator;
+        double right_back_power = (ly + lx - rx)/denominator;
+
+        left_front.setPower(left_front_power);
+        left_back.setPower(left_back_power);
+        right_front.setPower(right_front_power);
+        right_back.setPower(right_back_power);
+
+        /*
         left_front.setPower(ly + lx + rx);
         left_back.setPower(ly - lx + rx);
         right_front.setPower(ly - lx - rx);
         right_back.setPower(ly + lx - rx);
+        */
 
-        telemetry.addData("Left Front: ", ly + lx + rx);
-        telemetry.addData("Left Back: ", ly - lx + rx);
-        telemetry.addData("Right Front: ", ly - lx - rx);
-        telemetry.addData("Right Back: ", ly + lx - rx);
+
+        telemetry.addData("Left Front: ", left_front_power);
+        telemetry.addData("Left Back: ", left_back_power);
+        telemetry.addData("Right Front: ", right_front_power);
+        telemetry.addData("Right Back: ", right_back_power);
         telemetry.addData("Runtime: ", runtime.toString());
         telemetry.update();
 
