3a1628d010b329c90e2c4f868d53d9d18139c04d
DrUnlimitedGG <94410378+DrUnlimitedGG@users.noreply.github.com>
2021-11-20 18:23:10 -0600

    Update MecanumDrive.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumDrive.java
@@ -17,8 +17,8 @@ public class MecanumDrive extends OpMode {
     public DcMotorEx left_back;
     public DcMotorEx carousel;
 
-    private ElapsedTime runtime = new ElapsedTime();
-
+    private ElapsedTime runtime = new ElapsedTime(ElapsedTime.Resolution.SECONDS);
+    private double encoderConstant = 89.1267682333;
 
     @Override
     public void init() {
@@ -34,9 +34,6 @@ public class MecanumDrive extends OpMode {
             right_back.setDirection(DcMotorEx.Direction.REVERSE);
             left_front.setDirection(DcMotorEx.Direction.FORWARD);
             left_back.setDirection(DcMotorEx.Direction.FORWARD);
-
-
-
 
     }
 
@@ -64,58 +61,49 @@ public class MecanumDrive extends OpMode {
      */
     @Override
     public void loop() {
-        double lx = 1.1*gamepad1.left_stick_x;
+        double lx = gamepad1.left_stick_x;
         double ly = -gamepad1.left_stick_y;
         double rx = gamepad1.right_stick_x;
 
         double denominator = Math.max(Math.abs(ly) + Math.abs(lx) + Math.abs(rx), 1);
 
-        double left_front_power = (ly + lx + rx)/denominator;
-        double left_back_power = (ly - lx + rx)/denominator;
-        double right_front_power = (ly - lx - rx)/denominator;
-        double right_back_power = (ly + lx - rx)/denominator;
+        double left_front_power = (ly + lx + rx) / denominator;
+        double left_back_power = (ly - lx + rx) / denominator;
+        double right_front_power = (ly - lx - rx) / denominator;
+        double right_back_power = (ly + lx - rx) / denominator;
 
-        left_front.setPower(left_front_power);
-        left_back.setPower(left_back_power);
-        right_front.setPower(right_front_power);
-        right_back.setPower(right_back_power);
+        left_front.setPower(left_front_power / 2);
+        left_back.setPower(left_back_power / 2);
+        right_front.setPower(right_front_power / 2);
+        right_back.setPower(right_back_power / 2);
 
-        /*
-        left_front.setPower(ly + lx + rx);
-        left_back.setPower(ly - lx + rx);
-        right_front.setPower(ly - lx - rx);
-        right_back.setPower(ly + lx - rx);
-        */
-
-        while (gamepad1.dpad_right) {
-            carousel.setPower(0.25);
-        }
-
-        while (gamepad1.dpad_left) {
-            carousel.setPower(0.25);
-        }
-
-
-        telemetry.addData("Left Front: ", left_front_power);
+        /*telemetry.addData("Left Front: ", left_front_power);
         telemetry.addData("Left Back: ", left_back_power);
         telemetry.addData("Right Front: ", right_front_power);
         telemetry.addData("Right Back: ", right_back_power);
         telemetry.addData("Runtime: ", runtime.toString());
-        telemetry.update();
+        telemetry.update(); */
 
+        while (gamepad1.right_bumper == true) {
+            carousel.setVelocity(encoderConstant);
 
+            if (gamepad1.right_bumper == false) {
+                carousel.setPower(0);
+                break;
+            }
+
+        }
 
 
     }
 
-    /*
-     * Code to run ONCE after the driver hits STOP
-     */
-    @Override
-    public void stop() {
+        /*
+         * Code to run ONCE after the driver hits STOP
+         */
+        @Override
+        public void stop () {
+
+        }
 
     }
-}
-
-
 
