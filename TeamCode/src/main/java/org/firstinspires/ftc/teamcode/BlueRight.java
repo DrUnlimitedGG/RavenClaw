70ccb1e7935464cec4a22aa3576ee641fb2d5cfa
adityasaran <epicplayzquestions@gmail.com>
2022-01-15 13:11:48 -0500

    Revert "we know you're the sus impostor and i saw you vent in electrical"
    This reverts commit 8b8fc881

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BlueRight.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BlueRight.java
@@ -29,8 +29,6 @@
 
 package org.firstinspires.ftc.teamcode;
 
-import android.graphics.Camera;
-
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
@@ -40,15 +38,6 @@ import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.util.ElapsedTime;
 import com.qualcomm.robotcore.util.Range;
-
-import org.firstinspires.ftc.robotcore.external.Telemetry;
-import org.opencv.core.Core;
-import org.opencv.core.Mat;
-import org.opencv.core.Point;
-import org.opencv.core.Rect;
-import org.opencv.core.Scalar;
-import org.opencv.imgproc.Imgproc;
-import org.openftc.easyopencv.OpenCvPipeline;
 
 @Autonomous(name="BlueRight", group = "Blue")
 // @Disabled
@@ -62,14 +51,10 @@ public class BlueRight extends LinearOpMode {
     private DcMotorEx RF = null;
     private DcMotorEx RB = null;
     private DcMotorEx carousel = null;
-    private DcMotorEx viper = null;
-    private Camera Camera = null;
 
     private final double encoderConstant = 45.2847909695;
 
     private final double carouselConstant = 45.8366237;
-
-    private final double viperConstant = 40.151561;
 
 
     @Override
@@ -85,9 +70,6 @@ public class BlueRight extends LinearOpMode {
         RF = hardwareMap.get(DcMotorEx.class, "right_front");
         RB = hardwareMap.get(DcMotorEx.class, "right_back");
         carousel = hardwareMap.get(DcMotorEx.class, "sustainable");
-        viper = hardwareMap.get(DcMotorEx.class, "Sussy Wussy");
-        Camera = hardwareMap.get(Camera.class, "amogOS");
-
 
 
         // Most robots need the motor on one side to be reversed to drive forward
@@ -105,7 +87,6 @@ public class BlueRight extends LinearOpMode {
         if (opModeIsActive()) {
             telemetry.addData("Running: ", "Blue Right");
             telemetry.update();
-            DuckDetector();
 
             driveForward(18, 100);
             Thread.sleep(100);
@@ -113,7 +94,7 @@ public class BlueRight extends LinearOpMode {
             turnLeft(3, 200);
             Thread.sleep(100);
 
-            viperExtend(10);
+            // code for viper slides here
 
             turnRight(10, 100);
             Thread.sleep(100);
@@ -286,106 +267,4 @@ public class BlueRight extends LinearOpMode {
         Thread.sleep((long) (time * 1000));
         carousel.setVelocity(0);
     }
-
-    public void viperExtend(double distance) throws InterruptedException {
-        viper.setMode(DcMotorEx.RunMode.STOP_AND_RESET_ENCODER);
-        viper.setTargetPosition((int) (distance * viperConstant));
-        viper.setMode(DcMotorEx.RunMode.RUN_TO_POSITION);
-        viper.setVelocity(1);
-
-        while (viper.isBusy()) {
-            idle();
-        }
-
-        viper.setPower(0);
-    }
-    public static class DuckDetector extends OpenCvPipeline {
-        Telemetry telemetry;
-        Mat mat = new Mat();
-        public enum Location {
-            LEFT,
-            RIGHT,
-            MIDDLE,
-            NOT_FOUND
-        }
-        private org.firstinspires.ftc.teamcode.DuckDetector.Location location;
-
-        final Rect LEFT_ROI = new Rect(
-                new Point(90, 75),
-                new Point(150, 115));
-        final Rect MIDDLE_ROI = new Rect(
-                new Point(160,75),
-                new Point(220,115)
-        );
-        final Rect RIGHT_ROI = new Rect(
-                new Point(230, 75),
-                new Point(290, 115));
-        double PERCENT_COLOR_THRESHOLD = 0.03;
-
-        public DuckDetector(Telemetry t) { telemetry = t; }
-
-        public Mat processFrame(Mat input) {
-            Imgproc.cvtColor(input, mat, Imgproc.COLOR_RGB2HSV);
-            Scalar lowHSV = new Scalar(140, 50, 69);
-            Scalar highHSV = new Scalar(160, 255, 255);
-
-            Core.inRange(mat, lowHSV, highHSV, mat);
-
-            Mat left = mat.submat(LEFT_ROI);
-            Mat right = mat.submat(RIGHT_ROI);
-            Mat middle = mat.submat(MIDDLE_ROI);
-
-            double leftValue = Core.sumElems(left).val[0] / LEFT_ROI.area() / 255;
-            double rightValue = Core.sumElems(right).val[0] / RIGHT_ROI.area() / 255;
-            double middleValue = Core.sumElems(middle).val[0] / MIDDLE_ROI.area() / 255;
-
-            left.release();
-            right.release();
-
-            telemetry.addData("Left raw value", (int) Core.sumElems(left).val[0]);
-            telemetry.addData("Right raw value", (int) Core.sumElems(right).val[0]);
-            telemetry.addData("Middle raw value", (int) Core.sumElems(middle).val[0]);
-            telemetry.addData("Left percentage", Math.round(leftValue * 100) + "%");
-            telemetry.addData("Right percentage", Math.round(rightValue * 100) + "%");
-            telemetry.addData("Middle percentage", Math.round(middleValue * 100) + "%");
-
-            boolean isLeft = leftValue > PERCENT_COLOR_THRESHOLD;
-            boolean isRight = rightValue > PERCENT_COLOR_THRESHOLD;
-            boolean isMiddle = middleValue > PERCENT_COLOR_THRESHOLD;
-
-            if (!isLeft && !isRight && !isMiddle) {
-                location = org.firstinspires.ftc.teamcode.DuckDetector.Location.NOT_FOUND;
-                telemetry.addData("Duck Location", "not found");
-            }
-            else if (isLeft){
-                location = org.firstinspires.ftc.teamcode.DuckDetector.Location.LEFT;
-                telemetry.addData("Duck Location", "left");
-            }
-            else if (isMiddle) {
-                location = org.firstinspires.ftc.teamcode.DuckDetector.Location.MIDDLE;
-                telemetry.addData("Duck Location", "middle");
-            }
-            else {
-                location = org.firstinspires.ftc.teamcode.DuckDetector.Location.RIGHT;
-                telemetry.addData("Duck Location", "right");
-            }
-            telemetry.update();
-
-            Imgproc.cvtColor(mat, mat, Imgproc.COLOR_GRAY2RGB);
-
-            Scalar colorStone = new Scalar(255, 0, 0);
-            Scalar colorSkystone = new Scalar(0, 255, 0);
-
-            Imgproc.rectangle(mat, LEFT_ROI, location == org.firstinspires.ftc.teamcode.DuckDetector.Location.LEFT? colorSkystone:colorStone);
-            Imgproc.rectangle(mat, RIGHT_ROI, location == org.firstinspires.ftc.teamcode.DuckDetector.Location.RIGHT? colorSkystone:colorStone);
-            Imgproc.rectangle(mat, MIDDLE_ROI, location == org.firstinspires.ftc.teamcode.DuckDetector.Location.MIDDLE? colorSkystone:colorStone);
-
-            return mat;
-        }
-
-        public org.firstinspires.ftc.teamcode.DuckDetector.Location getLocation() {
-            return location;
-        }
-    }
-
 }
